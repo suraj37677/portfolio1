21d84be2ed562b9a424ca79899ce4d88e7d8f5de
suraj kumar sahoo <surajkumarsahoo379@gmail.com>
2023-08-23 10:34:11 -0700

    partfolio

---
+++ src/utils/data.js
@@ -0,0 +1,158 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "Website Design",
+    projects: 15,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Mobile App Design",
+    projects: 4,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+  {
+    name: "Brand Identity",
+    projects: 7,
+    icon: MdWorkspacesOutline,
+    bg: "#F26440",
+  },
+];
+
+export const WhatDoIHelp = [
+  "I will help you with finging a solution and solve your problem, I use process design to create digital products. Besides that also help their business.",
+  "I use process design to create digital products. Besides that also help their business",
+];
+
+export const workExp = [
+  {
+    place: "Self-Employed,Ambala",
+    tenure: "Aug 2019 - May 2020",
+    role: "Digital Marketing",
+    detail:
+      "Digital marketing is the practice of promoting products, services, or brands using digital channels and technologies. It encompasses various online platforms and strategies to reach a target audience, build brand awareness, drive traffic, and generate leads or sales.",
+  },
+  {
+    place: "Wipro infrastructure of Engineering,Banglore",
+    tenure: "jan 2021 - Nov 2021",
+    role: "Production Engineer Trainee",
+    detail:
+      "I'll learn about the manufacturing processes, machinery, and technologies used in Wipro Infrastructure Engineering's production. This might include understanding how products are assembled, tested, and quality controlled.",
+  },
+  {
+    place: "Apsis Technology pvt Ltd",
+    tenure: "jan2022 - june2023",
+    role: "Jr.Software Developer",
+    detail:
+      "Developing user interfaces using React.js, a JavaScript library for building interactive and dynamic UI components.Implementing responsive designs and ensuring a seamless user experience across devices and screen sizes"
+      
+  },
+      
+];
+
+export const comments = [
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people1.png",
+  },
+  {
+    name: "Anamika Sandula",
+    post: "Creative Manager",
+    comment:
+      "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    img: "./people2.png",
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
